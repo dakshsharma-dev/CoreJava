c17e5ad1acf50d075dec8ec9b4feb920a3eb3c0d
Daksh Sharma <dakshsharma1016@gmail.com>
2026-09-26 13:03:34 +0530

    Functional interface done

--- src/_06_Interface/_02_Functional_InterfaceOrSAM.java
+++ src/_06_Interface/_02_Functional_InterfaceOrSAM.java
@@ -1,4 +1,31 @@
 package _06_Interface;
 
+// FUNCTIONAL INTERFACE ----> SAM(Single Abstract Method)  ----> has exactly 1 abstract method
+
+@FunctionalInterface
+interface func{
+    void show();
+}
+
+// Way 1 of using show() method
+class funcHelper implements func{
+    public void show(){
+        System.out.println("In show of funcHelper");
+    }
+}
+
 public class _02_Functional_InterfaceOrSAM {
+    public static void main(String[] args) {
+        func fn1 = new funcHelper();
+        fn1.show();                                                    // In show of funcHelper
+
+        // Way 2 of using show() method using anonymous inner class
+        func fn2 = new func(){
+            public void show(){
+                System.out.println("In show of func");
+            }
+        };
+        // now to make the above code shorter and cleaner ----> lambda expression comes into picture (to make JAVA less verbose{verbose -> using more words than are necessary to express an idea})
+        fn2.show();                                                    // In show of func
+    }
 }
